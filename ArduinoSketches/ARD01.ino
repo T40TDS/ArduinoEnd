f4ee5a77cff77e29598f3bcd31eedcdbd4541954
T40TDS <ahadrizvi@gmail.com>
2015-08-13 20:49:59 -0400

    Update ARD01.ino

--- ArduinoSketches/ARD01.ino
+++ ArduinoSketches/ARD01.ino
@@ -1,9 +1,12 @@
-boolean portOpen = false;
-int portType = -1;
 /*portType index: -1 = unconfigured, 1 = generic serial message, 2 = lcd serial message*/
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+boolean portOpen = false;
+int portType = -1;
+
 void setup(){
-  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
@@ -30,25 +33,25 @@ void scanSerial(){
 }
 
 void processSerial(String message){
-  if(portOpen = true && portType = 2){
+  if(portOpen == true && portType == 2){
     lcdWrite(message);
   }
   
-  if(portOpen = true && portType = 1){
-    #ProccessingInstructionsForGenericSerialMessage
+  if(portOpen == true && portType == 1){
+    //ProccessingInstructionsForGenericSerialMessage
   }
   
-  if(portOpen = False){
-    if(message.equals("BgSrMsg"){
+  if(portOpen == false){
+    if(message.equals("BgSrMsg")){
       portOpen = true;
     }
   }
   
-  if (portOpen = true && portType = -1){
-      if(message.equals("SERIAL-GSM"){
+  if (portOpen == true && portType == -1){
+      if(message.equals("SERIAL-GSM")){
         portType = 1;
       }
-      if(message.equals("SERIAL-LCD"){
+      if(message.equals("SERIAL-LCD")){
         portType = 2;
       }
   }
@@ -58,7 +61,7 @@ void processSerial(String message){
 void lcdWrite(String message){
      char inputC[message.length()-1];
      for(int i = 0; i <= message.length()-1; i++){
-      inputC[i] = input.charAt(i);
+      inputC[i] = message.charAt(i);
      }
              
      for(int i = 0; i <= message.length()-1; i++){
